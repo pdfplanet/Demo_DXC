32ccda689ae97a9383707d961fbf09ef13f03da9
Sumit Kumar <81169016+pdfplanet@users.noreply.github.com>
2022-09-29 15:16:48 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,10 +1,6 @@
 
-class HelloWorld {
+class hello {
     public static void main(String[] args) {
-    int a ,b;
-    a = 300;
-    b= 200;
-        System.out.println(a+b); 
         System.out.println("Hello, World!"); 
     }
 }
