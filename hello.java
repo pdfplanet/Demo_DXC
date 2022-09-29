a08993b727e80fec6928ac4ca449c7815d3790d6
Sumit Kumar <81169016+pdfplanet@users.noreply.github.com>
2022-09-29 15:08:42 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,10 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+    int a ,b;
+    a = 300;
+    b= 200;
+        System.out.println(a+b); 
+        System.out.println("Hello, World!"); 
+    }
+}
